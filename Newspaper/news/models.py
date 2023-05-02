2e46fbe5fe9d538f21614fe668db018c15774a73
Am0789 <122796268+Am0789@users.noreply.github.com>
2023-05-02 12:50:53 +0300

    newspaper

--- Newspaper/news/models.py
+++ Newspaper/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 reader = 're'
@@ -11,13 +12,11 @@ POSITIONS = [
     (post, 'статья')
 ]
 
-class Post(models.Model):  #использовать метод Preview?
+class Post(models.Model):
     Post_data = models.DateField(auto_now_add=True) #время размещения поста
-    Post = models.CharField(max_length=500)
+    post_name = models.CharField(max_length=255, default='noname')
     Post = models.CharField(max_length=2, choices=POSITIONS)#поле выбора ст.новость
-    like = models.BooleanField(default=False)
-    dislike = models.BooleanField(default=False)
-    cost = models.FloatField(default = 0.0)
+    post_rating = models.IntegerField(default=0)
     Post = models.ForeignKey('Author', on_delete=models.CASCADE)#связь «один ко многим» с моделью Author
     Post = models.ManyToManyField('Category', through = 'PostCategory')
 
@@ -28,31 +27,39 @@ class PostCategory(models.Model):
 
 
 class Category(models.Model): #категория статьи или новости
-    Category = models.CharField(max_length=2, choices=POSITIONS)
-    Category = models.CharField(default = ' ')
-    Category = models.CharField(max_length=50)
-
-    unique = True
+        name = models.CharField(max_length=200, unique=True)
 
 
 class Comment(models.Model):
-    Comment_data = models.DateField(auto_now_add=True) #дата и время создания комментария
-    Comment = models.ForeignKey('Post', on_delete=models.CASCADE)#связь «один ко многим» с моделью Post
-    Comment = models.ForeignKey('User', on_delete=models.CASCADE)#связь «один ко многим» со встроенной моделью User
-    Comment = models.CharField(max_length=500)#текст комментария
-#рейтинг комментария
+    comment_data = models.DateField(auto_now_add=True) #дата и время создания комментария
+    comment = models.ForeignKey('Post', on_delete=models.CASCADE)#связь «один ко многим» с моделью Post
+    comment = models.ForeignKey('User', on_delete=models.CASCADE)#связь «один ко многим» со встроенной моделью User
+    comm_text = models.CharField(max_length=500)#текст комментария
+    rating = models.SmallIntegerField(default=0)
 
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
 
 class Author(models.Model):
-    Author = models.CharField(max_length=2, choices=POSITIONS)
-    name = models.CharField(max_length=50)
-    Author = models.OneToOneField('User', on_delete=models.CASCADE)
-    Author = models.ForeignKey('Post', on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
-    #update_rating()
+    user_name = models.CharField(max_length=30, default='noname')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        post_rating = self.post.post_rating * 3
+        user_rating = self.user_rating
+        summ = int(post_rating +com_rating + user_rating)
+        return summ
+
 
 class User(models.Model):
-    User = models.CharField(max_length=2, choices=POSITIONS)
+    user_name = models.CharField(max_length=30, default='user')
+    user = models.CharField(max_length=2, choices=POSITIONS)
     password = models.CharField(('password'), max_length=128)
     last_login = models.DateTimeField(('last login'), blank=True, null=True)
 
