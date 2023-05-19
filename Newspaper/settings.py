6df86e5b791d20c421b26e49f929b5b93e08bf95
Am0789 <122796268+Am0789@users.noreply.github.com>
2023-05-20 01:13:01 +0300

    Add files via upload

---
+++ Newspaper/settings.py
@@ -0,0 +1,166 @@
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-tgqdh@_kc_+ec$w)93_g0+3ru3zmkd$+6-*uyag5=rl_4)4woj'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    'news.apps.NewsConfig',
+    'accounts',
+    'sign',
+    'protect',
+    'django_filters',
+    'django_apscheduler',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+
+]
+
+SITE_ID = 1
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+]
+
+ROOT_URLCONF = 'Newspaper.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'Newspaper.wsgi.application'
+LOGIN_REDIRECT_URL = '/news/'
+LOGIN_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/news/'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+
+ACCOUNT_FORMS = {'signup': 'news.forms.BasicSignupForm'}
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# ADMINS = [
+#     ('yadimka7', 'yadimka7@gmail.com'),
+#     # список всех админов в формате ('имя', 'их почта')
+# ]
+# # это будет у нас вместо аргумента FROM в массовой рассылке
+# SERVER_EMAIL = 'Koreshkov0789d@yandex.ru'
+
+
+SITE_URL = 'http//127.0.0.1:8000'
+
+ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+MAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
+EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
+EMAIL_HOST_USER = 'Koreshkov0789d'  # ваше имя пользователя, user@yandex.ru, сюда надо писать user
+EMAIL_HOST_PASSWORD = 'g2U-XmA-Qva-7nu'  # пароль от почты
+EMAIL_USE_SSL = True  # Яндекс использует ssl, включать его здесь обязательно
+DEFAULT_FROM_EMAIL = 'Koreshkov0789d@yandex.ru'
+APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
+APSCHEDULER_RUN_NOW_TIMEOUT = 25
+
+CELERY_BROKER_URL = 'redis://default:da0BF64Z0fH5NsMRWFVownLvut4frjx5@redis-15997.c299.asia-northeast1-1.gce.cloud' \
+                    '.redislabs.com:15997'
+CELERY_RESULT_BACKEND = 'redis://default:da0BF64Z0fH5NsMRWFVownLvut4frjx5@redis-15997.c299.asia-northeast1-1.gce' \
+                        '.cloud.redislabs.com:15997'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
---
+++ news/urls.py
@@ -0,0 +1,26 @@
+from allauth.account.views import LoginView, LogoutView
+from django.urls import path
+
+from .views import PostDetail, PostsList, PostCreateViews, PostUpdateViews, PostDeleteViews, upgrade_me, \
+    BaseRegisterView, SearchPostListView, subscribe, unsubscribe, CategoryListView
+
+urlpatterns = [
+    path('', PostsList.as_view()),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('create/', PostCreateViews.as_view(), name='post_create'),
+    path('<int:pk>/update/', PostUpdateViews.as_view(), name='post_update'),
+    path('<int:pk>/delete/', PostDeleteViews.as_view(), name='post_delete'),
+    path('article/create/', PostCreateViews.as_view()),
+    path('article/<int:pk>/update/', PostUpdateViews.as_view()),
+    path('article/<int:pk>/delete/', PostDeleteViews.as_view()),
+    path('signup/', BaseRegisterView.as_view(template_name='sign/signup.html'), name='signup'),
+    path('search/', SearchPostListView.as_view()),
+    path('login/', LoginView.as_view(template_name='sign/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='sign/logout.html'), name='logout'),
+    path('upgrade/', upgrade_me, name='upgrade'),
+    path('category/<int:pk>/', CategoryListView.as_view(), name='category_list'),
+    path('category/<int:pk>/subscribe/', subscribe, name='subscribe'),
+    path('article/<int:pk>/update/', PostUpdateViews.as_view()),
+    path('article/<int:pk>/delete/', PostUpdateViews.as_view()),
+    path('category/<int:pk>/unsubscribe/', unsubscribe, name='unsubscribe'),
+]
