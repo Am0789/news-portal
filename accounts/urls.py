c647954b6a2255a9a59bf8f5c8f4278b85544426
Am0789 <122796268+Am0789@users.noreply.github.com>
2023-05-20 01:08:35 +0300

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserUpdateView, upgrade_me, unsubscribe
-
-
-urlpatterns = [
-    path('account/', UserUpdateView.as_view(), name='personal_area'),
-    path('upgrade/', upgrade_me, name='upgrade'),
-    path('unsubscribe/', unsubscribe, name='unsubscribe'),
-]
